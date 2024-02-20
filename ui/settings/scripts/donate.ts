9f14a1f70ec8b0c3e9f9a04ca078b18856d80e6d
Marc Beyer <marc.beyer1@gmx.de>
2024-02-20 21:35:37 +0000

    Updated donate dialog

--- ui/settings/scripts/donate.ts
+++ ui/settings/scripts/donate.ts
@@ -2,17 +2,19 @@ const donateButton = document.getElementById("donate-btn") as HTMLButtonElement;
 const donateButtonPaypal = document.getElementById("donate-btn-paypal") as HTMLButtonElement;
 const donateButtonPatreon = document.getElementById("donate-btn-patreon") as HTMLButtonElement;
 const closeDonationButton = document.getElementById("close-donation-btn") as HTMLButtonElement;
-const donationContainer = document.getElementById("donation-container") as HTMLDivElement;
+const donationDialog = document.getElementById("donation-dialog") as HTMLDialogElement;
 
 const paypalDonationUrl = "https://www.paypal.com/donate/?hosted_button_id=KYJWDCH3ZJ4U2";
 const patreonUrl = "https://www.patreon.com/time_machine_development";
 
 export function initDonation() {
     donateButton.addEventListener("click", () => {
-        donationContainer.classList.add("open");
+        donationDialog.showModal();
+        //donationContainer.classList.add("open");
     });
     closeDonationButton.addEventListener("click", () => {
-        donationContainer.classList.remove("open");
+        donationDialog.close();
+        //donationDialog.classList.remove("open");
     });
     donateButtonPaypal.addEventListener("click", () => {
         window.open(paypalDonationUrl, "_blank");
